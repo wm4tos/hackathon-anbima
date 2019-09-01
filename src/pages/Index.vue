29f47885bb85d87a24c69105e9ad74925208c5bc
Wesley Matos <wesley.matos@redfox.tech>
2019-09-01 04:13:26 -0300

    redirect to welcome page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,12 @@
 <template>
-  <q-page>
+  <q-page class="flex justify-center items-center">
+    <router-link :to="{ name: 'welcome' }">
+      <img
+        src="~assets/i9acao.svg"
+        alt="i9ação"
+        height="195.38px"
+      >
+    </router-link>
   </q-page>
 </template>
 
