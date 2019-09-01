90e664ff33d6bb5063ae333743362e20cf737050
Wesley Matos <wesley.matos@redfox.tech>
2019-09-01 05:24:57 -0300

    made interrogation component

---
+++ src/components/interrogation-button.vue
@@ -0,0 +1,44 @@
+<template>
+  <q-btn
+    :class="`bg-${colorButton} text-${colorText}`"
+    label="?"
+    round
+    size="lg"
+    @click="changeTooltipValue"
+    @blur="changeTooltipValue"
+  >
+    <q-tooltip
+      v-model="tooltip"
+      anchor="bottom right"
+    >
+      {{ labelTooltip }}
+    </q-tooltip>
+  </q-btn>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    tooltip: false,
+  }),
+  props: {
+    labelTooltip: {
+      type: String,
+      default: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, animi nobis facere libero repellendus quasi! Nulla qui labore, expedita cum assumenda sequi sint fugit sunt repellat doloribus temporibus illo minus.',
+    },
+    colorButton: {
+      type: String,
+      default: 'black',
+    },
+    colorText: {
+      type: String,
+      default: 'white',
+    },
+  },
+  methods: {
+    changeTooltipValue() {
+      this.tooltip = !this.tooltip;
+    },
+  },
+};
+</script>
